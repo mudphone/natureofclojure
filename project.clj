f20c03ad0a5791f483e4251ca2c1b182631c5229
Kyle Oba <koba@mudphone.com>
2013-02-02 06:55:46 -1000

    Add basic intro namespace for random walker and update project.clj.
    Getting ready to produce our first example sketch.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject natureofclojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :description "A bunch of Clojure Quil examples based on Daniel Shiffman's Nature of Code video channel on Vimeo."
+  :url "http://github.com/mudphone/natureofclojure"
+  :license {:name "Create Commons Attribution-ShareAlike 2.0 Generic License"
+            :url "http://creativecommons.org/licenses/by-sa/2.0/"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [quil "1.6.0"]])
---
+++ src/natureofclojure/intro-1/random-walker.clj
@@ -0,0 +1,6 @@
+(ns natureofclojure.intro-1.random-walker)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
