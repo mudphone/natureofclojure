e56927e26428fd604efdf50837e86d432cff54e1
Kyle Oba <koba@mudphone.com>
2013-02-04 02:54:47 -1000

    Split walker-specific code into separate namespace.
    The intent here is to use this pattern to define additional walkers.
    The setup/draw stuff should remain fairly boilerplate.

--- src/natureofclojure/i_1/random_walk_traditional.clj
+++ src/natureofclojure/i_1/random_walk_traditional.clj
@@ -6,23 +6,8 @@
 ;; https://github.com/shiffman/The-Nature-of-Code-Examples/tree/master/Processing/introduction/NOC_I_1_RandomWalkTraditional
 ;;
 (ns natureofclojure.i-1.random-walk-traditional
-  (:require [quil.core :as qc]))
-
-(def WIDTH 800)
-(def HEIGHT 600)
-(def walker (atom {:x (/ WIDTH 2.0)
-                   :y (/ HEIGHT 2.0)}))
-
-(defn step-walker [w-atom]
-  (let [choice (rand-int 4)]
-    (cond (= choice 0) (swap! w-atom update-in [:x] #(qc/constrain-float (+ % 1) 0 WIDTH))
-          (= choice 1) (swap! w-atom update-in [:x] #(qc/constrain-float (- % 1) 0 WIDTH))
-          (= choice 2) (swap! w-atom update-in [:y] #(qc/constrain-float (+ % 1) 0 HEIGHT))
-          (= choice 3) (swap! w-atom update-in [:y] #(qc/constrain-float (- % 1) 0 HEIGHT)))))
-
-(defn render-walker-at-position [position]
-  (qc/stroke 0)
-  (qc/point (:x position) (:y position)))
+  (:require [quil.core :as qc]
+            [natureofclojure.i-1.walker.traditional :as wt]))
 
 (defn setup []
   (qc/frame-rate 60)
@@ -30,8 +15,8 @@
   (qc/smooth))
 
 (defn draw []
-  (step-walker walker)
-  (render-walker-at-position @walker))
+  (wt/step-walker wt/walker)
+  (wt/render-walker @wt/walker))
 
 (defn run []
   (qc/defsketch random-walk-traditional
---
+++ src/natureofclojure/i_1/walker/traditional.clj
@@ -0,0 +1,36 @@
+;; Based on the Nature of Code
+;; by Daniel Shiffman
+;; http://natureofcode.com
+;;
+;; Specifically:
+;; https://github.com/shiffman/The-Nature-of-Code-Examples/tree/master/Processing/introduction/NOC_I_1_RandomWalkTraditional
+;;
+(ns natureofclojure.i-1.walker.traditional
+  (:require [quil.core :as qc]))
+
+(def WIDTH 800)
+(def HEIGHT 600)
+
+(def walker (atom {:x (/ WIDTH 2.0)
+                   :y (/ HEIGHT 2.0)}))
+
+(defn render-walker [w]
+  (qc/stroke 0)
+  (qc/point (:x w) (:y w)))
+
+(defn step-walker [w-atom]
+  (let [choice (rand-int 4)]
+    (cond (= choice 0) (swap! w-atom update-in [:x] #(qc/constrain-float (+ % 1) 0 WIDTH))
+          (= choice 1) (swap! w-atom update-in [:x] #(qc/constrain-float (- % 1) 0 WIDTH))
+          (= choice 2) (swap! w-atom update-in [:y] #(qc/constrain-float (+ % 1) 0 HEIGHT))
+          (= choice 3) (swap! w-atom update-in [:y] #(qc/constrain-float (- % 1) 0 HEIGHT)))))
+
+
+
+
+
+
+
+
+
+
