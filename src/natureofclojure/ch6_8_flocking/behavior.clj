2ed441a659ea7fc9ec5250a1acd5cabf9f313811
Kyle Oba <koba@mudphone.com>
2014-09-28 13:03:12 -1000

    Add first flocking example: seek + separate

---
+++ src/natureofclojure/ch6_8_flocking/behavior.clj
@@ -0,0 +1,89 @@
+;; Based on the Nature of Code
+;; by Daniel Shiffman
+;; http://natureofcode.com
+;;
+(ns natureofclojure.ch6-8-flocking.behavior
+  (:require
+   [quil.core :as q]
+   [quil.middleware :as m]
+   [natureofclojure.math.fast-vector :as fvec]))
+
+(defn dist-vehicle [loc vehicles]
+  (mapv (fn [v]
+          (let [d (fvec/distance (:location v) loc)]
+            [d v]))
+        vehicles))
+
+(defn apply-force [vehicle f-vector]
+  (update-in vehicle [:acceleration] #(fvec/+ % f-vector)))
+
+(defn between-0 [upper-d d]
+  (and (> d 0.0)
+       (< d upper-d)))
+
+(defn seek
+  "Returns the seek force, which must be applied."
+  [target vehicle]
+  (let [{:keys [location max-force max-speed velocity]} vehicle]
+    (-> (fvec/- target location)
+        (fvec/normalize)
+        (fvec/* max-speed)
+        (fvec/- velocity)
+        (fvec/limit max-force))))
+
+(defn separate
+  "Returns separation force, based on neighbor distance and desired
+  separation distance.  Force must be applied to a vehicles accel."
+  [sep-dist all vehicle]
+  (let [{:keys [location max-force max-speed velocity]} vehicle
+        dist-veh (doall
+                  (->> all
+                       (dist-vehicle location)
+                       (filter (fn [[d _]] (between-0 sep-dist d)))))
+        num-vehicles (count dist-veh)]
+    (if (< 0 num-vehicles)
+      (let [sum-dir (doall
+                     (reduce (fn [avg-dir [d v]]
+                               (let [diff (-> (fvec/- location (:location v))
+                                              (fvec/normalize)
+                                              (fvec// d))]
+                                 (fvec/+ avg-dir diff)))
+                             (fvec/fvec 0 0) dist-veh))]
+        (-> sum-dir
+            (fvec/normalize)
+            (fvec/set-mag max-speed)
+            (fvec/- velocity)
+            (fvec/limit max-force)))
+      (fvec/fvec 0.0 0.0))))
+
+(defn borders
+  [edge-x edge-y vehicle-r vehicle]
+  "Assumes multiple path segments (at least two)."
+  (let [{:keys [location]} vehicle
+        [x y] (fvec/x-y location)]
+    (cond->
+     vehicle
+     (> x (+ edge-x vehicle-r))
+     (assoc-in [:location]
+               (fvec/fvec (- 0 vehicle-r) y))
+
+     (< x (- 0 vehicle-r))
+     (assoc-in [:location]
+               (fvec/fvec (+ edge-x vehicle-r) y))
+
+     (> y (+ edge-y vehicle-r))
+     (assoc-in [:location]
+               (fvec/fvec x (- 0 vehicle-r)))
+
+     (< y (- 0 vehicle-r))
+     (assoc-in [:location]
+               (fvec/fvec x (+ edge-y vehicle-r))))))
+
+(defn move-vehicle [vehicle]
+  (let [v (fvec/+ (:velocity vehicle) (:acceleration vehicle))
+        v (fvec/limit v (:max-speed vehicle))
+        loc (fvec/+ v (:location vehicle))]
+   (-> vehicle
+       (assoc-in [:velocity] v)
+       (assoc-in [:location] loc)
+       (assoc-in [:acceleration] (fvec/fvec 0 0)))))
---
+++ src/natureofclojure/ch6_8_flocking/separation_seek.clj
@@ -0,0 +1,97 @@
+;; Based on the Nature of Code
+;; by Daniel Shiffman
+;; http://natureofcode.com
+;;
+;; Specifically:
+;; 
+;;
+(ns natureofclojure.ch6-8-flocking.separation-seek
+  (:require
+   [quil.core :as q]
+   [quil.middleware :as m]
+   [natureofclojure.math.fast-vector :as fvec]
+   [natureofclojure.ch6-8-flocking.behavior :as beh]))
+
+(def SIZE-W 800.0)
+(def SIZE-H 600.0)
+
+(def VEHICLE-R 10.0)
+(def VEHICLE-D (* 2 VEHICLE-R))
+
+(def SEPARATION-DIST 30)
+
+(defn random-v-comp []
+  (let [r (+ 20.0 (rand 40.0))]
+    (if (> (rand 1.0) 0.5)
+      (* -1 r)
+      r)))
+
+(defn random-vehicle
+  ([]
+     (random-vehicle (rand SIZE-W) (rand SIZE-H)))
+  ([x y]
+     {:location (fvec/fvec x y)
+      :velocity (fvec/fvec (random-v-comp)
+                           (random-v-comp))
+      :acceleration (fvec/fvec 0.0 0.0)
+      :max-speed 3.0
+      :max-force 0.2}))
+
+(def VEHICLES
+  (vec
+   (for [_ (range 10)]
+     (random-vehicle))))
+
+(defn setup []
+  {:vehicles VEHICLES})
+
+(defn apply-behaviors [all vehicle]
+  (let [sep-factor 2.0
+        seek-factor 1.0        
+        sep-force (fvec/*
+                   (beh/separate SEPARATION-DIST all vehicle)
+                   sep-factor)
+        target (fvec/fvec (q/mouse-x) (q/mouse-y)) 
+        seek-force (fvec/*
+                    (beh/seek target vehicle)
+                    seek-factor)]
+    (-> vehicle
+        (beh/apply-force sep-force)
+        (beh/apply-force seek-force))))
+
+(defn update-vehicles [vehicles]
+  (doall (map #(->> %
+                    (apply-behaviors vehicles)
+                    (beh/move-vehicle)
+                    (beh/borders SIZE-W SIZE-H VEHICLE-R))
+              vehicles)))
+
+(defn update [{:keys [vehicles] :as state}]
+  (-> state
+      (update-in [:vehicles] update-vehicles)))
+
+(defn draw-vehicle
+  [{:keys [location]}]
+  (let [[x y] (fvec/x-y location)]
+    (q/with-translation [x y]
+      (q/ellipse 0 0 VEHICLE-D VEHICLE-D))))
+
+(defn draw [state]
+  (q/background 0)
+  (let [{:keys [vehicles]} state]
+    (q/stroke 0.5)
+    (q/fill 180)
+    (doall (map draw-vehicle vehicles))))
+
+(defn mouse-dragged [state event]
+  (let [{:keys [x y]} event]
+    (update-in state [:vehicles] #(conj % (random-vehicle x y)))))
+
+(q/defsketch quil-workflow
+  :title "Group Steering Behaviors: Separation"
+  :size [SIZE-W SIZE-H]
+  :setup setup
+  :update update
+  :draw draw
+  :mouse-dragged mouse-dragged
+  :middleware [m/fun-mode])
